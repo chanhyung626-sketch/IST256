547286e50c90c35f2581e06090d0ae8186b2b026
David <david.snaxx@gmail.com>
2026-02-24 19:33:02 -0500

    Created first iteration of conference entry form

---
+++ js/conference-entry.js
@@ -0,0 +1,13 @@
+// form field elements
+const $entryId = $('#entryId');
+const $title = $('#title');
+const $description = $('#description');
+const $category = $('input[name="category"]');
+const $format = $('input[name="format"]');
+const $entryPrice = $('#entryPrice');
+const $additionalInfo = $('#additionalInfo');
+
+// form input alerts
+const $duplicateAlert = $('#duplicate-alert');
+const $successAlert = $('#success-alert');
+
