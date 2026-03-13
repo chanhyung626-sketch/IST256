a7667105ab8da669074a2a4d7914e16d590037b0
David <david.snaxx@gmail.com>
2026-03-13 12:45:55 -0400

    Added function for updating product entries and added documentation to product.js

--- js/products/products.js
+++ js/products/products.js
@@ -28,6 +28,10 @@ export function loadProducts() {
     return JSON.parse(localStorage.getItem('products')) || [];
 }
 
+/**
+ * Overwrites the products object array in local storage with the provided product list.
+ * @param products An array of {@link products} to take the place of the 'products' key.
+ */
 export function saveProducts(products) {
     localStorage.setItem('products', JSON.stringify(products));
 }
@@ -40,4 +44,18 @@ export function addProduct(product) {
     let products = loadProducts();
     products.push(product);
     saveProducts(products);
+}
+
+/**
+ * Modifies an existing {@link Product} if one exists in local storage.
+ * @param updatedProduct The new version of the {@link Product} object.
+ * @return {boolean} True if a matching product was found and updated. False if there was no preexisting product.
+ */
+export function updateProduct(updatedProduct) {
+    let products = loadProducts();
+    const index = products.findIndex(p => String(p.id) === String(updatedProduct.id));
+    if (index === -1) return false;
+    products[index] = updatedProduct;
+    saveProducts(products);
+    return true;
 }
